2ba28296924ee091a13824946d16f9f256721856
Ben Merken <11701910@student.pxl.be>
2019-03-11 10:59:36 +0100

    Minor progression on AES. Created enum AESKeySize for valid AES key sizes.

--- src/main/java/be/pxl/basicSecurity/appDev/AESKeySize.java
+++ src/main/java/be/pxl/basicSecurity/appDev/AESKeySize.java
@@ -1,4 +1,18 @@
 package be.pxl.basicSecurity.appDev;
 
 public enum AESKeySize {
+
+    SIZE_128(128),
+    SIZE_192(192),
+    SIZE_256(256);
+
+    private int length;
+
+    private AESKeySize(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
--- src/main/java/be/pxl/basicSecurity/appDev/SymmetricCryptoApp.java
+++ src/main/java/be/pxl/basicSecurity/appDev/SymmetricCryptoApp.java
@@ -2,20 +2,38 @@ package be.pxl.basicSecurity.appDev;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Scanner;
 
-// TODO Create enum for bit length of key (only 128, 192 and 256 bit length allowed)
+/**
+ * Application class that encrypts and decrypts a specified file, using AES
+ * and 3DES symmetric encryption algorithms
+ */
 
 public class SymmetricCryptoApp {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             System.out.print("Give me a path to a file: ");
             String filePath = in.nextLine();
+            System.out.print("Give me a name for the output file: ");
+            String outputFileName = in.next();
+
+            Path inputFile = Paths.get(filePath);
+            Path outputFile = inputFile.getParent().resolve(outputFileName);
+            SecretKey key = generateRandomAESKey(AESKeySize.SIZE_256);
+            IvParameterSpec iv = generateInitVector();
+            encryptAES(key, iv, inputFile, outputFile);
+
             System.out.println();
             System.out.println("Encrypted text AES: " /*+ encryptAES()*/); //TODO uncoomment method call when implemented
             System.out.println("Decrpyted text AES:" /*+ decryptAES()*/); //TODO uncoomment method call when implemented
@@ -26,45 +44,69 @@ public class SymmetricCryptoApp {
         }
     }
 
-    // Decrypt AES
+
+    /**
+     * Decrypt a file, specified by a given path
+     *
+     * @param key        The AES key being used
+     * @param initVector The initialization vector being used
+     * @param path       Specifies the path to the file to decrypt
+     * @throws
+     */
 
     // TODO Implement method
-    private static String decryptAES(String key, String initVector, String cipherText) {
+    private static String decryptAES(String key, String initVector, String path) {
         throw new NotImplementedException();
     }
 
-    // Encrypt AES
+    /**
+     * Encrypt a file in AES, utilizing cipher block chaining and PKCS5 padding specification
+     *
+     * @param key        The AES key being used
+     * @param initVector The initialization vector being used
+     * @param inputFile  Specifies the path to the file to encrypt
+     * @param outputFile Specifies the path to the location of the encrypted file
+     * @throws NoSuchAlgorithmException           When a particular cryptographic algorithm is requested
+     *                                            but is not available in the environment
+     * @throws NoSuchPaddingException             When the specified padding pattern isn't available in the
+     *                                            environment
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     */
 
     // TODO Implement method
-    private static String encryptAES(String key, String initVector, String plain) {
+    private static String encryptAES(SecretKey key, IvParameterSpec initVector, Path inputFile, Path outputFile) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException {
+        if (!key.getAlgorithm().equals("AES")) {
+            throw new NoSuchAlgorithmException();
+        }
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, key, initVector);
         throw new NotImplementedException();
     }
 
     /**
      * Generates a random AES key
      *
-     * @param length Length of the key in bits
+     * @param length Length of the key in bits, specified by an instance of enum AESKeySize
      * @throws NoSuchAlgorithmException When a particular cryptographic algorithm is requested
      *                                  but is not available in the environment
      */
 
-    private static SecretKey generateRandomAESKey(int length) throws NoSuchAlgorithmException {
+    private static SecretKey generateRandomAESKey(AESKeySize length) throws NoSuchAlgorithmException {
         KeyGenerator kg = KeyGenerator.getInstance("AES");
-        kg.init(length);
+        kg.init(length.getLength(), new SecureRandom());
         return kg.generateKey();
     }
 
     /**
      * Generates a random initialization vector for the initial AES CBC cipher block
-     *
-     * @param length The length of the AES key in bits. Used to generate an
-     *               initialization vector of equivalent length
-     * @throws
+     * for block size = 16 bytes
      */
-    // TODO Implement method
-    private static IvParameterSpec generateInitVector(int length) {
+
+    private static IvParameterSpec generateInitVector() {
         SecureRandom random = new SecureRandom();
-        byte[] bytes = new byte[length / 8];
+        byte[] bytes = new byte[16];
         random.nextBytes(bytes);
         return new IvParameterSpec(bytes);
     }
