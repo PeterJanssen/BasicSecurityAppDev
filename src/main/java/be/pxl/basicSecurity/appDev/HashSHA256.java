45f64ffc62178b0f37d21d8df4c368960d4b0f0f
Ben Merken <11701910@student.pxl.be>
2019-03-19 11:21:55 +0100

    Add rudimentary SHA-256 functionality to HashSHA256.java
    As of now, the HashSHA256 class' generateHash() method prints a SHA-256 hash for a file put as the inputFile Path parameter. This is not the intended behaviour, and only meant for testing purposes. The right behaviour will be added next commit

--- src/main/java/be/pxl/basicSecurity/appDev/HashSHA256.java
+++ src/main/java/be/pxl/basicSecurity/appDev/HashSHA256.java
@@ -1,5 +1,49 @@
 package be.pxl.basicSecurity.appDev;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Generates a SHA-256 hash
+ */
+
 public class HashSHA256 {
-    
+
+    private HashSHA256() {
+    }
+
+    /**
+     * Generates a SHA-256 hash, and writes it to
+     *
+     * @param inputFile  The path to file of which a hash is generated
+     * @param outputFile The path to the file to which the has is written
+     */
+
+    public static void generateHash(Path inputFile, Path outputFile) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        try (DigestInputStream dis = new DigestInputStream(
+                new FileInputStream(inputFile.toFile()), md)
+        ) {
+            while (dis.read() != -1) {
+                md = dis.getMessageDigest();
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        System.out.println(result.toString());
+
+    }
+
+
+    public static String readHash(Path file) {
+        throw new NotImplementedException();
+    }
 }
