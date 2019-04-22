832aab49b01d1862251dc551f50bc92df2a487ad
PeterJanssen <PeterJanssen@users.noreply.github.com>
2019-04-22 21:28:11 +0200

    Fix mutliple window issue
    Added a method in the controller that closes the current stage that the pressed button is contained in.
    
    Fixes #1

--- src/main/java/be/pxl/basicSecurity/appDev/crypto/Controller.java
+++ src/main/java/be/pxl/basicSecurity/appDev/crypto/Controller.java
@@ -2,8 +2,10 @@ package be.pxl.basicSecurity.appDev.crypto;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,27 +13,32 @@ import java.io.IOException;
 
 public class Controller {
 
+
     @FXML
-    public void OpenHomePage() throws IOException {
+    public void OpenHomePage(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartScreen.fxml"));
+        closeStageOfNode((Node) mouseEvent.getSource());
         setSceneAndRoot(root, "CryptoApp");
     }
 
     @FXML
-    public void OpenDecryptionPage() throws IOException {
+    public void OpenDecryptionPage(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/DecryptionScreen.fxml"));
+        closeStageOfNode((Node) mouseEvent.getSource());
         setSceneAndRoot(root, "Decryption Form");
     }
 
     @FXML
-    public void OpenEncryptionKeyGenerationPage() throws IOException {
+    public void OpenEncryptionKeyGenerationPage(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/EncryptionKeyGenScreen.fxml"));
+        closeStageOfNode((Node) mouseEvent.getSource());
         setSceneAndRoot(root, "EncryptionKey Form");
     }
 
     @FXML
-    public void OpenEncryptionMessagePage() throws IOException {
+    public void OpenEncryptionMessagePage(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/EncryptionMsgScreen.fxml"));
+        closeStageOfNode((Node) mouseEvent.getSource());
         setSceneAndRoot(root, "EncryptionMessage Form");
     }
 
@@ -41,6 +48,12 @@ public class Controller {
         Stage stage = new Stage(StageStyle.DECORATED);
         stage.setTitle(title);
         stage.setScene(scene);
+
         stage.show();
     }
+
+    private void closeStageOfNode(Node node) {
+        ((Stage) (node).getScene().getWindow()).close();
+    }
+
 }
